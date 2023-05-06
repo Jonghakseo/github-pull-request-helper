8783f6b25f38e3e5ebf824a5041709f1bc211b55
Jonghakseo <jonghak.seo@creatrip.com>
2023-05-06 20:38:33 +0900

    init project

--- manifest.ts
+++ manifest.ts
@@ -8,17 +8,9 @@ const manifest: chrome.runtime.ManifestV3 = {
   name: packageJson.name,
   version: packageJson.version,
   description: packageJson.description,
-  options_page: "src/pages/options/index.html",
-  background: {
-    service_worker: "src/pages/background/index.js",
-    type: "module",
-  },
   action: {
     default_popup: "src/pages/popup/index.html",
     default_icon: "icon-34.png",
-  },
-  chrome_url_overrides: {
-    newtab: "src/pages/newtab/index.html",
   },
   icons: {
     "128": "icon-128.png",
@@ -31,7 +23,6 @@ const manifest: chrome.runtime.ManifestV3 = {
       css: ["assets/css/contentStyle<KEY>.chunk.css"],
     },
   ],
-  devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
