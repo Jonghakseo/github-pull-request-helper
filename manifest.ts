4eaf54125a9cac417c42dea8779f823e352cd1a8
Jonghakseo <jonghak.seo@creatrip.com>
2023-05-07 02:40:47 +0900

    update content matches

--- manifest.ts
+++ manifest.ts
@@ -22,7 +22,7 @@ const manifest: chrome.runtime.ManifestV3 = {
   },
   content_scripts: [
     {
-      matches: ["http://*/*", "https://*/*", "<all_urls>"],
+      matches: ["https://github.com/*"],
       js: ["src/pages/content/index.js"],
       // KEY for cache invalidation
       css: ["assets/css/contentStyle<KEY>.chunk.css"],
