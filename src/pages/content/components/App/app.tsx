728dd5969d718bb7a6e6bc1e6932ef5f3178714e
Jonghakseo <jonghak.seo@creatrip.com>
2023-05-06 21:06:41 +0900

    page 감지 로직 추가

--- src/pages/content/components/App/app.tsx
+++ src/pages/content/components/App/app.tsx
@@ -1,9 +1,31 @@
-import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import useUrlChangeEffect from "@pages/content/components/hooks/useUrlChangeEffect";
 
 export default function App() {
-  useEffect(() => {
-    console.log("content view loaded");
-  }, []);
+  const notify = () =>
+    toast("PR page!", {
+      position: "top-right",
+      autoClose: false,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
-  return <div className="content-view">content view</div>;
+  useUrlChangeEffect((currentUrl) => {
+    if (!isPRPage(currentUrl)) {
+      return;
+    }
+    notify();
+  });
+
+  return <ToastContainer />;
 }
+
+const PR_PAGE_REGEX = new RegExp(
+  "https://github.com/([a-zA-Z0-9-_]+)/([a-zA-Z0-9-_]+)/pull/(\\d+)"
+);
+
+const isPRPage = (url: string) => PR_PAGE_REGEX.test(url);
