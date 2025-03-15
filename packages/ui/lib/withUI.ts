927b533d2ef608d1710d777fbbbcf9b0529ef56f
Jonghakseo <unqocn@gmail.com>
2025-03-15 18:45:00 +0900

    fix: line error

--- packages/ui/lib/withUI.ts
+++ packages/ui/lib/withUI.ts
@@ -1,6 +1,5 @@
 import deepmerge from 'deepmerge';
 import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
 import tailwindAnimate from 'tailwindcss-animate';
 
 export function withUI(tailwindConfig: Config): Config {
