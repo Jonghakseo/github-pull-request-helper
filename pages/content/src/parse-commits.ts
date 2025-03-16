e19c6ddac42abf2e88b1f6e0b88bd5a3587aa619
Jonghakseo <unqocn@gmail.com>
2025-03-16 19:12:49 +0900

    fix: 버그 수정

--- pages/content/src/parse-commits.ts
+++ pages/content/src/parse-commits.ts
@@ -3,13 +3,13 @@ export default function parseCommits() {
   const commits = Array.from(timelineBodies).slice(1);
 
   return commits.map(commit => {
-    const codes = commit.querySelectorAll('code');
+    const codes = Array.from(commit.querySelectorAll('code'));
     if (!codes.length) {
       throw Error('[Pull Request Commit Notify] Code not found');
       // return;
     }
-    const commitMessage = codes[0].textContent?.trim();
-    const commitLink = codes[1].querySelector('a')?.href;
+    const commitMessage = codes.at(0)?.textContent?.trim();
+    const commitLink = codes.at(-1)?.querySelector('a')?.href;
     const commitSha = commitLink?.split('/').pop();
     if (!commitLink || !commitMessage || !commitSha) {
       throw Error('[Pull Request Commit Notify] Commit message is not valid');
